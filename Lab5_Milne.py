ee7942556e1f6bd61a013d5cbec838be6b56a018
John Milne <44006020+JohnMilne@users.noreply.github.com>
2026-04-16 09:00:32 -0400

    Update Lab5_Milne.py
    Finished and finalized code, which was used to take the measurements.

--- Lab5_Milne.py
+++ Lab5_Milne.py
@@ -31,7 +31,7 @@
 from install import install
 
 # Holds the names of libraries to be installed
-libraries = ["pyvisa", "asyncio", "clickplc", "numpy", "pandas", "openpyxl"]
+libraries = ["pyvisa", "numpy", "pandas", "openpyxl"]
 
 # Following code loops the install function to download all requested libraries.
 for i in range(len(libraries)):
@@ -39,6 +39,7 @@ for i in range(len(libraries)):
 
 # Imports of Import
 from detect_Instruments import detect_instruments
+from math import sqrt
 from time import sleep
 import openpyxl
 import pandas as pd
@@ -48,16 +49,66 @@ import pyvisa
 # Set up the instruments
 [supply, fungen, dmm, oscope] = detect_instruments()
 
-# Variables
-frequency = 50
-
-# Set up the function generator to produce the required 5 volt sine wave at a given f.
-fungen.write(f"C1:BaSeWaVe WaVeTyPe,SINe,FRQ,{frequency}")
-sleep(1)
-fungen.write("C1:OUTPut ON")
-sleep(1)
-
-# Set up the oscilloscope to take a measurement.
-oscope.write("PACU RMS,C1")
+# Setup the initial conditions for the instruments.
+oscope.write("C1:CPL A1M")
 sleep(2)
-oscope.query(f"C1:PAVA? RMS")
+
+# Set up the DMM to take a measurement.
+dmm.read_termination = "\n"
+dmm.write_termination = "\n"
+dmm.write("CHDR OFF")
+sleep(2)
+
+# Variables
+counter = 0
+freqs = [50, 60, 120, 1000]
+
+# Create the Excel spreadsheet.
+# Change the name of the file between each run to show the Res/Cap used.
+filepath = ("./Group3Lab5Data2000-100.xlsx")
+workbook = openpyxl.Workbook()
+worksheet = workbook.active
+worksheet.title = "Group3Lab5Data"
+workbook.save(filepath)
+worksheet['A1'] = "Frequency"
+worksheet['B1'] = "Oscilloscope"
+worksheet['C1'] = "DMM"
+worksheet['D1'] = "Ripple"
+worksheet['E1'] = "Factor"
+workbook.save(filepath)
+
+# FOR-loop to take the measurements over several frequencies.
+for frequency in freqs:
+
+    # Set up the function generator to produce the required 5 volt sine wave at a given f.
+    fungen.write("C1:BSWV WVTP,SIN")
+    fungen.write(f"C1:BSWV FRQ,{frequency}")
+    fungen.write("C1:BSWV AMP,5")
+    fungen.write("C1:OUTP ON")
+
+    # Set up the oscilloscope to take a measurement.
+    oscope.write("PACU RMS,C1")
+    sleep(2)
+
+    # Take measurements.
+    Vrms = round(float(oscope.query(f"C1:PAVA? RMS").split(",")[1].strip("V\n")),6)
+    Vdc = round(float(dmm.query("MEAS:VOLT:DC?")),6)
+    print(f"Oscilloscope: {Vrms}\tDMM: {Vdc}")
+    Ripple = round(sqrt(((Vrms*Vrms) + (Vdc*Vdc)) - (Vdc*Vdc)),6)
+    Factor = round(Ripple/Vdc,6)
+    print(f"VRipple: {Ripple}\tRipple Factor: {Factor}")
+    Factors = pd.DataFrame(columns=["Frequency","Oscilloscope","DMM","VRipple","RFactor"])
+    worksheet[f"A{counter+2}"] = frequency
+    worksheet[f"B{counter+2}"] = Vrms
+    worksheet[f"C{counter+2}"] = Vdc
+    worksheet[f"D{counter+2}"] = Ripple
+    worksheet[f"E{counter+2}"] = Factor
+    workbook.save(filepath)
+
+    counter += 1
+    sleep(2)
+
+
+
+
+
